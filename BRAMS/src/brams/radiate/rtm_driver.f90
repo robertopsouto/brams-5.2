29d1f311b3225e2cdef9b22e08522f6b1149e6f1
Roberto P Souto <rpsouto@lncc.br>
2016-04-06 08:59:42 -0300

    nova atualizacao

--- BRAMS/src/brams/radiate/rtm_driver.f90
+++ BRAMS/src/brams/radiate/rtm_driver.f90
@@ -775,11 +775,7 @@ contains
    !$OMP BARRIER
    !$OMP MASTER
    !- integer
-   allocate(ipos  (nofcols));
-   if ( allocated(ipos) ) then 
-      print*, "ipos allocated"
-      ipos =0 !integer                     
-   end if
+   allocate(ipos  (nofcols))     ;ipos =0 !integer                     
    allocate(jpos  (nofcols))     ;jpos =0 !integer
    allocate(imask (nofcols))     ;imask=0 !integer
    !- real
@@ -871,8 +867,8 @@ contains
       
          ipos(noc)=i
          jpos(noc)=j
-         print *, "i, ipos(noc), tid: ", i, ipos(noc), omp_get_thread_num()
-         print *
+         !print *, "i, ipos(noc), tid: ", i, ipos(noc), omp_get_thread_num()
+         !print *
          
          !- 2d input data
          coszen(noc)= radiate_g(ngrid)%cosz(i,j)
